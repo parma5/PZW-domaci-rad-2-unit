01adc69bfa948ca2e604ad3e1adf5a3ec6111f85
parma5 <iparma00@fesb.hr>
2015-09-14 14:57:45 +0200

    First commit

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using dr2;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // constructor
+        [TestMethod]
+        public void ctorDayLowValue()
+        {
+            Date date = new Date(0, 3, 2010);
+            Console.WriteLine("Dan je {0}", date.day);
+        }
+
+        [TestMethod]
+        public void ctorDayHighValue()
+        {
+            Date date = new Date(50, 1, 2010);
+            Console.WriteLine("Dan je {0}", date.day);
+        }
+
+        [TestMethod]
+        public void ctorMonthLowValue()
+        {
+            Date date = new Date(2, 0, 2010);
+            Console.WriteLine("Mjesec je {0}", date.month);
+        }
+
+        [TestMethod]
+        public void ctorMonthHighValue()
+        {
+            Date date = new Date(2, 15, 2010);
+            Console.WriteLine("Mjesec je {0}", date.month);
+        }
+
+        [TestMethod]
+        public void ctorYearLowValue()
+        {
+            Date date = new Date(2, 5, -1);
+            Console.WriteLine("Godina je {0}", date.year);
+        }
+
+        // get month name
+        [TestMethod]
+        public void getMonthNameAllCases()
+        {
+            for (int i = 1; i < 13; i++)
+            {
+                Date date = new Date(2, i, 2010);
+                Console.WriteLine("Mjesec je {0}", date.getMonthName());
+            }
+        }
+
+        // number of remaining days
+        [TestMethod]
+        public void getNumberOfRemainingDaysInMonth28()
+        {
+            Date date = new Date(3, 2, 2013);
+            Console.WriteLine("Preostalo je {0} dana do kraja mjeseca", date.getNumberOfRemainingDaysInMonth());
+        }
+
+        [TestMethod]
+        public void getNumberOfRemainingDaysInMonth29()
+        {
+            Date date = new Date(3, 2, 2012);
+            Console.WriteLine("Preostalo je {0} dana do kraja mjeseca", date.getNumberOfRemainingDaysInMonth());
+        }
+
+        [TestMethod]
+        public void getNumberOfRemainingDaysInMonth30()
+        {
+            Date date = new Date(3, 6, 2013);
+            Console.WriteLine("Preostalo je {0} dana do kraja mjeseca", date.getNumberOfRemainingDaysInMonth());
+        }
+
+        [TestMethod]
+        public void getNumberOfRemainingDaysInMonth31()
+        {
+            Date date = new Date(3, 7, 2013);
+            Console.WriteLine("Preostalo je {0} dana do kraja mjeseca", date.getNumberOfRemainingDaysInMonth());
+        }
+
+        [TestMethod]
+        public void isLeapYear4()
+        {
+            Date date = new Date(15, 9, 2004);
+            if (date.isLeapYear())
+            {
+                Console.WriteLine("Godina je prijestupna");
+            }
+            else
+            {
+                Console.WriteLine("Godina nije prijestupna");
+            }
+        }
+
+        [TestMethod]
+        public void isLeapYear100()
+        {
+            Date date = new Date(15, 9, 2100);
+            if (date.isLeapYear())
+            {
+                Console.WriteLine("Godina je prijestupna");
+            }
+            else
+            {
+                Console.WriteLine("Godina nije prijestupna");
+            }
+        }
+
+        [TestMethod]
+        public void isLeapYear400()
+        {
+            Date date = new Date(15, 9, 2000);
+            if (date.isLeapYear())
+            {
+                Console.WriteLine("Godina je prijestupna");
+            }
+            else
+            {
+                Console.WriteLine("Godina nije prijestupna");
+            }
+        }
+
+        [TestMethod]
+        public void isLeapYearFalse()
+        {
+            Date date = new Date(15, 9, 1995);
+            if (date.isLeapYear())
+            {
+                Console.WriteLine("Godina je prijestupna");
+            }
+            else
+            {
+                Console.WriteLine("Godina nije prijestupna");
+            }
+        }
+    }
+}
---
+++ dr2 - unit testovi/Date.cs
@@ -0,0 +1,125 @@
+﻿namespace dr2
+{
+    public class Date
+    {
+        public int year { get; set; }
+        public int month { get; set; }
+        public int day { get; set; }
+
+        public Date(int day, int month, int year)
+        {
+            if (day > 0 && day < 32)
+            {
+                this.day = day;
+            }
+            else
+            {
+                this.day = 1;
+            }
+            if (month > 0 && month < 13)
+            {
+                this.month = month;
+            }
+            else
+            {
+                this.month = 1;
+            }
+            if (year > 0)
+            {
+                this.year = year;
+            }
+            else
+            {
+                this.year = 1;
+            }
+        }
+
+        public string getMonthName()
+        {
+            var monthName = "";
+            switch (this.month)
+            {
+                case 1:
+                    monthName = "siječanj";
+                    break;
+                case 2:
+                    monthName = "veljača";
+                    break;
+                case 3:
+                    monthName = "ožujak";
+                    break;
+                case 4:
+                    monthName = "travanj";
+                    break;
+                case 5:
+                    monthName = "svibanj";
+                    break;
+                case 6:
+                    monthName = "lipanj";
+                    break;
+                case 7:
+                    monthName = "srpanj";
+                    break;
+                case 8:
+                    monthName = "kolovoz";
+                    break;
+                case 9:
+                    monthName = "rujan";
+                    break;
+                case 10:
+                    monthName = "listopad";
+                    break;
+                case 11:
+                    monthName = "studeni";
+                    break;
+                case 12:
+                    monthName = "prosinac";
+                    break;
+            }
+            return monthName;
+        }
+
+        public int getNumberOfRemainingDaysInMonth()
+        {
+            if (this.month == 2 && this.isLeapYear() == false)
+            {
+                return 28 - this.day;
+            }
+            else if (this.month == 2 && this.isLeapYear() == true)
+            {
+                return 29 - this.day;
+            }
+            else if (this.month == 4 || this.month == 6 || this.month == 9 || this.month == 11)
+            {
+                return 30 - this.day;
+            }
+            else
+            {
+                return 31 - this.day;
+            }
+        }
+
+        public bool isLeapYear()
+        {
+            if (this.year % 4 == 0)
+            {
+                if (this.year % 100 != 0)
+                {
+                    return true;
+                }
+                else if (this.year % 400 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
